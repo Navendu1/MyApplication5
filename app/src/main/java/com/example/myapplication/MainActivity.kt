d7793cf8b1a6b7e2dd591a6762f08289ec8dc7ce
devop <pritam.zr@gmail.com>
2021-04-07 22:03:40 +0530

    mainactivity add onclick

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,14 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var  button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+   button = findViewById(R.id.button)
 
-//        hello navendu
+        button.setOnClickListener {
+            Toast.makeText(this, "hello fake app", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
