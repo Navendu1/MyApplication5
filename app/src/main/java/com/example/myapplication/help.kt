ef918cbcf8365fe0e16cb4873c9793ca90c7dbdc
devop <pritam.zr@gmail.com>
2021-04-09 16:03:00 +0530

    add refresh in help activity button with function but data are in demo and as of now help is not link with  any button
    future info -- help is change to how to redeem and present help data goes to redeem activity...

--- app/src/main/java/com/example/myapplication/help.kt
+++ app/src/main/java/com/example/myapplication/help.kt
@@ -2,20 +2,45 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 
 class help : AppCompatActivity() {
 
     lateinit var  textView: TextView
+    lateinit var  button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
 
+
+
         textView = findViewById(R.id.codeId)
+        button = findViewById(R.id.button6)
+        var count : Int = 1
 
+        val redeem1 = getString(R.string.redeem1)
+        val redeem2 = getString(R.string.redeem2)
+        val redeem3 = getString(R.string.redeem3)
+        textView.text =  redeem1
 
-        textView.text = "hello navendu"
+        button.setOnClickListener {
+            count +=1
+            when (count) {
+                1 -> textView.text = redeem1
+                2 -> {
+                    textView.text =  redeem2
+                }
+                3 -> textView.text = redeem3
+                else -> { // Note the block
+                    Toast.makeText(this, "no more data avilabe", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        }
 
     }
 }
