b1eb448a24ddec6fd9b75abcfc84b7aa7957427d
Navendu1 <60533502+Navendu1@users.noreply.github.com>
2021-04-10 12:19:07 +0530

    mack goraaddfullcontennewv1.2

--- app/src/main/java/com/example/myapplication/contean_freecalculator1.kt
+++ app/src/main/java/com/example/myapplication/contean_freecalculator1.kt
@@ -3,21 +3,34 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class contean_freecalculator1 : AppCompatActivity() {
 
     lateinit var  button: Button
+    lateinit var editText: EditText
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contean_freecalculator1)
 
 
+        editText = findViewById(R.id.editTextTextPersonName5)
         button = findViewById(R.id.button2)
 
+
         button.setOnClickListener {
+            var name = editText.editableText.toString()
             intent = Intent(applicationContext, gatstartthentextv::class.java)
+
+            intent.putExtra("name",name)
             startActivity(intent)
         }
+
     }
 }
--- app/src/main/java/com/example/myapplication/gatstartthentextv.kt
+++ app/src/main/java/com/example/myapplication/gatstartthentextv.kt
@@ -4,19 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class gatstartthentextv : AppCompatActivity() {
 
     lateinit var  button: Button
+    lateinit var textView: TextView
+//    lateinit var  textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gatstartthentextv)
 
         button = findViewById(R.id.button4)
+        textView = findViewById(R.id.textView11)
+
+    val name =intent.getStringExtra("name")
+            this.textView.text = "Result \n $name \n Please Waitiing for 72-120 hours, \n if not working please tay agen  "
+
 
         button.setOnClickListener {
             intent = Intent(applicationContext, lestActivity::class.java)
             startActivity(intent)
+
         }
+
     }
 }
